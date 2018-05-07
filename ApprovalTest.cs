e3013c15cc67ae91a06078854f2eb2d52d57c9c9
Woody Zuill <wzuill@yahoo.com>
2018-05-07 08:55:48 +0200

    Added all files

---
+++ ApprovalTest.cs
@@ -0,0 +1,47 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text;
+using ApprovalTests.Combinations;
+using NUnit.Framework.Constraints;
+
+namespace csharp
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    public class ApprovalTest
+    {
+       //[Test]
+//        public void ThirtyDays()
+//        {
+//            StringBuilder fakeoutput = new StringBuilder();
+//            Console.SetOut(new StringWriter(fakeoutput));
+//            Console.SetIn(new StringReader("a\n"));
+//
+//            Program.Main(new string[] { });
+//            String output = fakeoutput.ToString();
+//            Approvals.Verify(output);
+//        }
+
+        [Test]
+        public void CharacterizationTest()
+        {
+            string[] names = {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
+            int[] sellIns = {-1, 0, 1, 2, 5, 6, 7, 10, 11, 12, 49, 50, 51};
+            int[] qualitys = {-1, 0, 1, 2, 49, 50, 51};
+            CombinationApprovals.VerifyAllCombinations(GetItem, names, sellIns, qualitys);
+
+        }
+
+        private string GetItem(string name, int sellIn, int quality)
+        {
+            Item[] items = {new Item {Name = name, Quality = quality, SellIn = sellIn}};
+            new GildedRose(items).UpdateQuality();
+            return items[0].ToString();
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,15 @@
+﻿namespace csharp
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name:{Name}, Quality:{Quality}, SellIn: {SellIn}";
+        }
+        
+    }
+}
